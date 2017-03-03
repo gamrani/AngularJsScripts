178e30ca2405311bd872833cd3a8eca02124bd2c
gamrani <gamraniyoussef@gmail.com>
2017-03-03 14:07:40 +0000

    script is ready

---
+++ script.js
@@ -0,0 +1,47 @@
+var app = angular.module('app',[]);
+app.controller('ctrl',controller);
+var items = [
+  {
+    name : "Jhon",quantity :"50"
+  },{
+    name : "alberto", quantity : "60"
+  },{
+    name : "jese",quantity : "1000"
+  },{
+    name : "zak",quantity : "5985"
+  }
+];
+
+
+controller.inject=['$scope'];
+function controller($scope){
+
+
+    $scope.iteams=items;
+    $scope.valeur=false;
+    $scope.v=false;
+    $scope.add = function(){
+       $scope.valeur=true;
+    };
+    $scope.ajouter = function(){
+       var newItem = {
+         name : $scope.AddName ,
+         quantity: $scope.AddQuantity
+       };
+
+       $scope.iteams.push(newItem);
+
+    };
+    $scope.delete=function(){
+      $scope.v=true;
+    };
+    $scope.remove=function(){
+
+
+           var x = $scope.index-1;
+           $scope.iteams.splice(x,1);
+
+
+    };
+
+}
